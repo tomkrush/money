b0ecca6c47c4547673c1f46cc3d2dd9b8ffa3e9a
Tom Krush <tom@tomkrush.com>
2017-10-22 19:38:15 -0500

    The program can now accept a CSV file path and output the contents

--- money.go
+++ money.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("Money App")
+	path := flag.String("path", "", "Import CSV file from specified path")
+	flag.Parse()
+
+	file, err := os.Open(*path)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	for {
+		record, err := reader.Read()
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error: ", err)
+		}
+
+		fmt.Println("Record", record)
+	}
 }
