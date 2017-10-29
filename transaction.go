0402f23e880811d73daf02f3e382fc31c00e9164
Tom Krush <tom@tomkrush.com>
2017-10-29 10:00:14 -0500

    Updated the documentation for the transaction type

--- transaction.go
+++ transaction.go
@@ -2,13 +2,13 @@ package main
 
 import "time"
 
-// Transaction is represents a US currency transaction.
-// It is intented that all transactions reference a bank account.
-// The amount is measured in Penny's.
 // Currency is the amount of dollars represented in pennies.
 type Currency struct {
 	Amount int
 }
+
+// Transaction is the withdrawl and deposit of a currency from a
+// bank account on a specific date.
 type Transaction struct {
 	BankID        string
 	AccountNumber string
