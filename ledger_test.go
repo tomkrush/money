02efd0f819318a82f144b51f4f8097e851a2f0be
Tom Krush <tom@tomkrush.com>
2017-10-29 23:03:20 -0500

    Simplified test by removing transactionsWithAmounts
    I abstracted the methods in my tests too soon

--- ledger_test.go
+++ ledger_test.go
@@ -5,16 +5,6 @@ import (
 	"testing"
 )
 
-func transactionsWithAmounts(amounts ...int) Transactions {
-	var transactions Transactions
-
-	for _, a := range amounts {
-		transactions = append(transactions, Transaction{Amount: Currency{a}})
-	}
-
-	return transactions
-}
-
 func TestLedger_sum(t *testing.T) {
 	tests := []struct {
 		name         string
@@ -23,25 +13,39 @@ func TestLedger_sum(t *testing.T) {
 	}{
 		{
 			"Sum of transactions should be 1000",
-			transactionsWithAmounts(500, 500),
+			Transactions{
+				Transaction{Amount: Currency{500}},
+				Transaction{Amount: Currency{500}},
+			},
 			Currency{1000},
 		},
 		{
 			"Sum of transactions should be 750",
-			transactionsWithAmounts(250, 250, 250),
+			Transactions{
+				Transaction{Amount: Currency{250}},
+				Transaction{Amount: Currency{250}},
+				Transaction{Amount: Currency{250}},
+			},
 			Currency{750},
 		},
 		{
 			"Sum of transactions should be 90",
-			transactionsWithAmounts(5, 10, 75),
+			Transactions{
+				Transaction{Amount: Currency{5}},
+				Transaction{Amount: Currency{10}},
+				Transaction{Amount: Currency{75}},
+			},
 			Currency{90},
 		},
 		{
 			"Sum of transactions should be 1",
-			transactionsWithAmounts(1),
+			Transactions{
+				Transaction{Amount: Currency{1}},
+			},
 			Currency{1},
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			l := &Ledger{
