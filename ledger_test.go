09b788ee1cc3e6cc9b07c251c9f5b3b890aaba55
Tom Krush <tom@tomkrush.com>
2017-10-29 22:59:56 -0500

    Replaced fields type with transactions

--- ledger_test.go
+++ ledger_test.go
@@ -20,35 +20,35 @@ func TestLedger_sum(t *testing.T) {
 		transactions Transactions
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		want   Currency
+		name         string
+		transactions Transactions
+		want         Currency
 	}{
 		{
 			"Sum of transactions should be 1000",
-			fields{transactionsWithAmounts(500, 500)},
+			transactionsWithAmounts(500, 500),
 			Currency{1000},
 		},
 		{
 			"Sum of transactions should be 750",
-			fields{transactionsWithAmounts(250, 250, 250)},
+			transactionsWithAmounts(250, 250, 250),
 			Currency{750},
 		},
 		{
 			"Sum of transactions should be 90",
-			fields{transactionsWithAmounts(5, 10, 75)},
+			transactionsWithAmounts(5, 10, 75),
 			Currency{90},
 		},
 		{
 			"Sum of transactions should be 1",
-			fields{transactionsWithAmounts(1)},
+			transactionsWithAmounts(1),
 			Currency{1},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			l := &Ledger{
-				transactions: tt.fields.transactions,
+				transactions: tt.transactions,
 			}
 			if got := l.Sum(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Ledger.sum() = %v, want %v", got, tt.want)
