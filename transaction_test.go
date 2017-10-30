cd5aefbc02d997ae8d65b6e11eb7acf19ef650e6
Tom Krush <tom@tomkrush.com>
2017-10-29 23:49:00 -0500

    Moved currency test into separate file

--- transaction_test.go
+++ transaction_test.go
@@ -3,32 +3,6 @@ package main
 import "testing"
 import "time"
 
-func TestCurrency_CentsToDollars(t *testing.T) {
-	type fields struct {
-		Amount int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   string
-	}{
-		{"Convert 1 penny to $0.00", fields{1}, "$0.01"},
-		{"Convert 120 pennies to $1.20", fields{120}, "$1.20"},
-		{"Convert 1005 pennies to $10.05", fields{1005}, "$10.05"},
-		{"Convert 6050400 pennies to $60504.00", fields{6050400}, "$60504.00"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			currency := &Currency{
-				Amount: tt.fields.Amount,
-			}
-			if got := currency.FormatToDollars(); got != tt.want {
-				t.Errorf("Currency.CentsToDollars() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func transactionsWithIDs(ids ...int) Transactions {
 	var transactions Transactions
 
