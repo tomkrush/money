c21f203a0629c13d82d4505a97c5b1075ec5c74a
Tom Krush <tom@tomkrush.com>
2017-10-29 19:55:24 -0500

    Added documentatin to FormatToDollars method

--- transaction.go
+++ transaction.go
@@ -10,6 +10,8 @@ type Currency struct {
 	Amount int
 }
 
+// FormatToDollars acts on the Current Type. This method outputs the currency
+// amount in dollars. This method prepends the amount with the dollar sign.
 func (c *Currency) FormatToDollars() string {
 	return string('$') + strconv.FormatFloat(float64(c.Amount)/100, 'f', 2, 64)
 }
