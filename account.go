5fb42eef20418868370b9d878f2654414e841577
Tom Krush <tom@tomkrush.com>
2017-11-03 17:04:24 -0500

    Added more documentation to Account object

--- account.go
+++ account.go
@@ -1,9 +1,14 @@
 package main
 
+// Account can only contain transactions with the same account id.
+//
+// I should probably have a function that validates that all the account ids
+// match. Right now this behavior is assumed.
 type Account struct {
 	Transactions Transactions
 }
 
+// Sum on an Account takes the sum of the transactions plus the starting balance
 func (a Account) Sum() Currency {
 	startingBalance := a.StartingBalance().Amount
 	total := a.Transactions.Sum().Amount
@@ -11,6 +16,8 @@ func (a Account) Sum() Currency {
 	return Currency{startingBalance + total}
 }
 
+// StartingBalance is the balance of the first transaction minus the
+// transaction amount
 func (a *Account) StartingBalance() Currency {
 	t := a.Transactions[0]
 
