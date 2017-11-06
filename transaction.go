e14542a9d6871e327122e94c9b2642d5b6a1d719
Tom Krush <tom@tomkrush.com>
2017-11-05 19:39:41 -0600

    Updated Transaction with new fields
    A transaction can now store a user description, category, and need.
    
    The user description is an override for the description. This value will likely be assigned to this property by the rules system.

--- transaction.go
+++ transaction.go
@@ -8,15 +8,26 @@ import (
 // Transaction is the withdrawl and deposit of a currency from a
 // bank account on a specific date.
 type Transaction struct {
-	BankID        string
-	AccountNumber string
-	AccountType   string
-	Balance       currency.Currency
-	StartDate     time.Time
-	EndDate       time.Time
-	Type          string
-	Date          time.Time
-	Amount        currency.Currency
-	UniqueID      int
-	Description   string
+	BankID          string
+	AccountNumber   string
+	AccountType     string
+	Balance         currency.Currency
+	StartDate       time.Time
+	EndDate         time.Time
+	Type            string
+	Date            time.Time
+	Amount          currency.Currency
+	UniqueID        int
+	Description     string
+	UserDescription string
+	Category        string
+	Need            bool
+}
+
+func (t *Transaction) getDescription() string {
+	if t.UserDescription != "" {
+		return t.UserDescription
+	}
+
+	return t.Description
 }
