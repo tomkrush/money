4ac1a4de00323be0e2602082eede814f6a438a39
Tom Krush <tom@tomkrush.com>
2017-11-06 08:48:28 -0600

    Re-introduced sorting on transactions

--- finance/transactions.go
+++ finance/transactions.go
@@ -1,5 +1,7 @@
 package finance
 
+import "sort"
+
 // Transactions is a collection of type Transaction
 type Transactions []Transaction
 
@@ -29,4 +31,30 @@ func (t Transactions) Sum() Currency {
 	}
 
 	return NewCurrency(total)
+}
+
+// Sort exposes the StdLib Sort command without needing to import
+// the package.
+func (t *Transactions) Sort() {
+	sort.Sort(t)
+}
+
+func (t Transactions) Len() int {
+	return len(t)
+}
+
+func (t Transactions) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t Transactions) Less(i, j int) bool {
+	if t[i].Date.Before(t[j].Date) {
+		return true
+	}
+
+	if t[i].Date.After(t[j].Date) {
+		return false
+	}
+
+	return t[i].UniqueID < t[j].UniqueID
 }
--- finance/transactions_test.go
+++ finance/transactions_test.go
@@ -86,3 +86,60 @@ func TestTransactions_Sum(t *testing.T) {
 		})
 	}
 }
+
+func TestTransactions_Sort(t *testing.T) {
+	tests := []struct {
+		name         string
+		transactions Transactions
+		want         []int
+	}{
+		{
+			"Correct order of simple list",
+			Transactions{
+				createTransaction(2, "2017-01-02"),
+				createTransaction(1, "2017-01-01"),
+				createTransaction(3, "2017-01-05"),
+				createTransaction(4, "2017-01-10"),
+			},
+			[]int{1, 2, 3, 4},
+		},
+		{
+			"Correct order based on date. Out of order based on priority",
+			Transactions{
+				createTransaction(2, "2017-01-02"),
+				createTransaction(1, "2017-01-01"),
+				createTransaction(422, "2017-01-01"),
+				createTransaction(3, "2017-01-05"),
+				createTransaction(4, "2017-01-10"),
+				createTransaction(423, "2017-01-02"),
+				createTransaction(424, "2017-01-11"),
+			},
+			[]int{1, 422, 2, 423, 3, 4, 424},
+		},
+		{
+			"Correct order with negative numbers",
+			Transactions{
+				createTransaction(5, "2017-01-01"),
+				createTransaction(10, "2017-01-01"),
+				createTransaction(2, "2017-01-01"),
+				createTransaction(-10, "2017-01-01"),
+			},
+			[]int{-10, 2, 5, 10},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			transactions := tt.transactions
+
+			transactions.Sort()
+
+			for i, transaction := range transactions {
+				if tt.want[i] != transaction.UniqueID {
+
+					t.Errorf("Index of [%d] = %v, want %v", i, transaction.UniqueID, tt.want[i])
+				}
+			}
+		})
+	}
+}
