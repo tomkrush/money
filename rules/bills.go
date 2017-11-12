d32a2ac89077d0c25b5ec494f9aef60725cb044c
Tom Krush <tom@tomkrush.com>
2017-11-12 10:53:03 -0600

    Added method to Bills to calculate Goal amount
    The goal amount is the sum of all bill rule amounts.

--- rules/bills.go
+++ rules/bills.go
@@ -1,6 +1,20 @@
 package rules
 
+import (
+	"money/finance"
+)
+
 // Bills contains a list of bill rules
 type Bills struct {
 	Rules []TransactionRule
+}
+
+func (bills Bills) GoalAmount() finance.Currency {
+	amount := 0
+
+	for _, rule := range bills.Rules {
+		amount += rule.Bill.Amount.Amount
+	}
+
+	return finance.NewCurrency(amount)
 }
---
+++ rules/bills_test.go
@@ -0,0 +1,30 @@
+package rules
+
+import (
+	"money/finance"
+	"testing"
+)
+
+func createBillRule(amount int) TransactionRule {
+	return TransactionRule{
+		Bill: BillRule{
+			Amount: finance.NewCurrency(amount),
+		},
+	}
+}
+
+func TestRules_Bills(t *testing.T) {
+	bills := Bills{
+		Rules: []TransactionRule{
+			createBillRule(1000),
+			createBillRule(750),
+			TransactionRule{},
+		},
+	}
+
+	goalAmount := bills.GoalAmount()
+
+	if goalAmount.Amount != 1750 {
+		t.Error("Goal amount incorrect")
+	}
+}
