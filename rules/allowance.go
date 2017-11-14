7956aebe683571672385107a5d02a18727aece9c
Tom Krush <tom@tomkrush.com>
2017-11-13 21:01:57 -0600

    Added Allowance function
    Updated algorithm for calculating Allowance

--- rules/allowance.go
+++ rules/allowance.go
@@ -2,13 +2,12 @@ package rules
 
 import "money/finance"
 
-type Allowance struct {
-	Bills  Bills
-	Income Income
-}
+// Allowance is calculated based on the (total income + projected bill total
+// + all other expenses)
+func Allowance(income Income, bills Bills, transactions finance.Transactions) finance.Currency {
+	other := transactions.TotalExpenses().Subtract(bills.ActualAmount())
+	projectedAmount := bills.ProjectedAmount()
+	allowance := income.Amount.Add(other.Add(projectedAmount))
 
-func (a Allowance) Monthly() finance.Currency {
-	remaining := a.Income.Amount.Add(a.Bills.ProjectedAmount())
-
-	return remaining
+	return allowance
 }
--- rules/allowance_test.go
+++ rules/allowance_test.go
@@ -5,47 +5,48 @@ import (
 	"testing"
 )
 
-func TestAllowance_Allowance(t *testing.T) {
-	allowance := Allowance{
-		Income: Income{
-			Amount: finance.NewCurrency(100),
-		},
+func TestAllowance_IncomeOnly(t *testing.T) {
+	income := Income{
+		Amount: finance.NewCurrency(100),
 	}
 
-	monthly := allowance.Monthly()
+	bills := Bills{}
+	transactions := finance.Transactions{}
 
-	if monthly.Amount != 100 {
-		t.Errorf("Allowance incorrect %d %d", monthly.Amount, 100)
+	allowance := Allowance(income, bills, transactions)
+
+	if allowance.Amount != 100 {
+		t.Errorf("Allowance incorrect %d %d", allowance.Amount, 100)
 	}
 }
 
-func TestAllowance_IncludingBills(t *testing.T) {
-	allowance := Allowance{
-		Income: Income{
-			Amount: finance.NewCurrency(3000),
+func TestAllowance_Bills(t *testing.T) {
+	income := Income{
+		Amount: finance.NewCurrency(10000),
+	}
+
+	transactions := finance.Transactions{
+		finance.Transaction{
+			Description: "hello",
+			Amount:      finance.NewCurrency(-1200),
 		},
-		Bills: Bills{
-			Rules: []TransactionRule{
-				createBillRule("hello", 1000),
-				createBillRule("world", 750),
-				TransactionRule{},
-			},
-			Transactions: finance.Transactions{
-				finance.Transaction{
-					Description: "hello",
-					Amount:      finance.NewCurrency(1200),
-				},
-				finance.Transaction{
-					Description: "test",
-					Amount:      finance.NewCurrency(1500),
-				},
-			},
+		finance.Transaction{
+			Description: "test",
+			Amount:      finance.NewCurrency(-1500),
 		},
 	}
 
-	monthly := allowance.Monthly()
+	bills := Bills{
+		Rules: []TransactionRule{
+			createBillRule("hello", 1000),
+			createBillRule("world", 750),
+		},
+		Transactions: transactions,
+	}
 
-	if monthly.Amount != 1050 {
-		t.Errorf("Allowance incorrect %d %d", monthly.Amount, 1050)
+	allowance := Allowance(income, bills, transactions)
+
+	if allowance.Amount != 6550 {
+		t.Errorf("Allowance incorrect %d %d", allowance.Amount, 6550)
 	}
 }
