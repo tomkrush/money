00a8215815e4f97e80bc706121a7b5c67d0f1e51
Tom Krush <tom@tomkrush.com>
2017-11-13 21:09:26 -0600

    Replaced calculation of other with UnplannedExpenses

--- rules/allowance.go
+++ rules/allowance.go
@@ -5,9 +5,10 @@ import "money/finance"
 // Allowance is calculated based on the (total income + projected bill total
 // + all other expenses)
 func Allowance(income Income, bills Bills, transactions finance.Transactions) finance.Currency {
-	other := transactions.TotalExpenses().Subtract(bills.ActualAmount())
+	unplannedExpense := UnplannedExpenses(bills, transactions)
 	projectedAmount := bills.ProjectedAmount()
-	allowance := income.Amount.Add(other.Add(projectedAmount))
+
+	allowance := income.Amount.Add(unplannedExpense.Add(projectedAmount))
 
 	return allowance
 }
