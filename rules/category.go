e747cf329ff2530e492297b441f3b77a825ef0e1
Tom Krush <tom@tomkrush.com>
2017-11-26 12:59:42 -0600

    Moved CategoryList from reports package to rules

---
+++ rules/category.go
@@ -0,0 +1,58 @@
+package rules
+
+import (
+	"money/finance"
+	"sort"
+)
+
+// Category is a list item
+type Category struct {
+	Category string
+	Amount   finance.Currency
+	Need     bool
+}
+
+// CategoryList is a list of categories
+type CategoryList []Category
+
+func (list CategoryList) get(category string) (*Category, int) {
+	for i, c := range list {
+		if category == c.Category {
+			return &c, i
+		}
+	}
+
+	return &Category{}, -1
+}
+
+// Len returns the length of a category list
+func (list CategoryList) Len() int { return len(list) }
+
+// Swap swaps to category items
+func (list CategoryList) Swap(i, j int) { list[i], list[j] = list[j], list[i] }
+
+// Less compares category amounts to order them from largest to smallest
+func (list CategoryList) Less(i, j int) bool {
+	return list[i].Amount.Amount > list[j].Amount.Amount
+}
+
+func GetCategories(transactions finance.Transactions) CategoryList {
+	categories := CategoryList{}
+
+	for _, t := range transactions {
+		category, i := categories.get(t.GetCategory())
+
+		if i >= 0 {
+			categories[i].Amount = categories[i].Amount.Add(t.Amount)
+		} else {
+			category.Category = t.GetCategory()
+			category.Need = t.Need
+			category.Amount = finance.NewCurrency(t.Amount.Amount)
+			categories = append(categories, *category)
+		}
+	}
+
+	sort.Sort(categories)
+
+	return categories
+}
---
+++ rules/category_test.go
@@ -0,0 +1,57 @@
+package rules
+
+import (
+	"money/finance"
+	"reflect"
+	"testing"
+)
+
+func TestCategory_GetCategory(t *testing.T) {
+	tests := []struct {
+		name         string
+		transactions finance.Transactions
+		want         CategoryList
+	}{
+		{
+			"Add up categories",
+			finance.Transactions{
+				finance.Transaction{
+					Category: "Gas",
+					Amount:   finance.NewCurrency(5000),
+				},
+				finance.Transaction{
+					Category: "Gas",
+					Amount:   finance.NewCurrency(2500),
+				},
+				finance.Transaction{
+					Category: "Gas",
+					Amount:   finance.NewCurrency(2500),
+				},
+				finance.Transaction{
+					Category: "Shopping",
+					Amount:   finance.NewCurrency(5000),
+				},
+			},
+			CategoryList{
+				Category{
+					Category: "Gas",
+					Amount:   finance.NewCurrency(10000),
+				},
+				Category{
+					Category: "Shopping",
+					Amount:   finance.NewCurrency(5000),
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			categories := GetCategories(tt.transactions)
+
+			if !reflect.DeepEqual(categories, tt.want) {
+				t.Errorf("Categories returned %v, want %v", categories, tt.want)
+			}
+		})
+	}
+}
